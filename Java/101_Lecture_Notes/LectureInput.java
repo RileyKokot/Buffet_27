784c086946e10244ca667e86129d3f2cc817bc55
RileyKokot <rileykokot@gmail.com>
2026-09-15 17:58:00 +0000

    update

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -4,6 +4,15 @@
 
 class LectureInput{
     public static void main(String args[]) {
-        // Your Code Goes here!
+    Scanner sc new Scanner (Sysytem.in);
+    String menuheading1 = new String("Hello welcome to hand boys restraunt");
+    String menuheading2 = new String("Heres what we got 😎");
+    String menuitem1 = new String("1. salmon and avacoado handroll: ");
+    double item1price = 12.00;
+    String menuitem2 = new String("2. Tuna and lemon handroll: ");
+    double item2price = 11.50;
+    String menuitem3= new String("3. Ell and ell sauce sushi");
+    double item3price = 10.00
+
 	}
 }
